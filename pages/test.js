addb1ff5435448af8e25f38e9734176e0cac0fb7
keita <keitakei777@gmail.com>
2024-01-07 14:53:58 +0900

    repair4

---
+++ pages/test.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+function UserList() {
+    const [users, setUsers] = useState([]);
+  
+    useEffect(() => {
+      // FlaskのAPIエンドポイントを指定します
+      const apiEndpoint = 'https://projectprojectkeita.azurewebsites.net/incomes';
+  
+      fetch(apiEndpoint)
+        .then(response => response.json())
+        .then(data => setUsers(data))
+        .catch(error => console.error('Error:', error));
+    }, []);
+  
+
+
+    return (
+        <div>
+      {users.map((user) => (
+        <div key={user.id}>
+          <h2>{user.name}</h2>
+          <p>年齢: {user.age}</p>
+          <p>ID: {user.id}</p>
+        </div>
+      ))}
+    </div>
+    );
+}
+
+export default UserList;
