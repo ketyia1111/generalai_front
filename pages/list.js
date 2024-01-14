766f9deefee0350ad7900bb8f32ae42381e7b280
keita <keitakei777@gmail.com>
2024-01-15 00:46:25 +0900

    goodcondition8

--- pages/list.js
+++ pages/list.js
@@ -2,11 +2,12 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import styles from '../styles/List.module.css';
 import Navbar from '../components/Navbar';
-import Image from 'next/image';  // 追加
+import Image from 'next/image';
 
 const ListPage = () => {
     const [items, setItems] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
+    const [base64Images, setBase64Images] = useState({});
 
     const formatDate = (dateString) => {
         const options = { year: 'numeric', month: 'long', day: 'numeric' };
@@ -14,14 +15,35 @@ const ListPage = () => {
     };
 
     const fetchData = async (page) => {
-        const result = await axios.get(`https://projectprojectkeita.azurewebsites.net/api/list/${page}`);  // あなたのAPIエンドポイントに変更してください
+        const result = await axios.get(`https://projectprojectkeita.azurewebsites.net/api/list/${page}`);
         setItems(result.data.items);
         setCurrentPage(page);
     };
 
+    const fetchImage = async (imageName) => {
+        const imageUrl = `https://projectkeita.blob.core.windows.net/images/${imageName}`;
+
+        axios.get(imageUrl, { responseType: 'arraybuffer' })
+            .then(response => {
+                const base64 = Buffer.from(response.data, 'binary').toString('base64');
+                setBase64Images(prevState => ({ ...prevState, [imageName]: base64 }));
+            })
+            .catch(error => {
+                console.error('Error fetching image:', error);
+            });
+    };
+
     useEffect(() => {
         fetchData(currentPage);
-    }, [currentPage]);  // currentPageを依存配列に追加
+    }, [currentPage]);
+
+    useEffect(() => {
+        items.forEach(item => {
+            if (!base64Images[item.image_name]) {
+                fetchImage(item.image_name);
+            }
+        });
+    }, [items]);
 
     return (
         <div>
@@ -31,7 +53,7 @@ const ListPage = () => {
                 {items.map((item) => (
                     <div key={item.id} className={styles.post}>
                         <h3>{formatDate(item.created_at)} {item.username}さんの投稿</h3>
-                        <Image src={`https://projectkeita.blob.core.windows.net/images/${item.image_name}`} alt="Diary image" width={500} height={300} />  {/* <img>タグを<Image>コンポーネントに置き換え、widthとheightを指定 */}
+                        {base64Images[item.image_name] && <Image src={`data:image/jpeg;base64,${base64Images[item.image_name]}`} alt="Diary image" width={500} height={300} />}
                         <p className={styles.text}>{item.diaries}</p>
                         <hr/>
                     </div>
