8da2945745cad05372c3c2b09951de6b6e31edfe
keita <keitakei777@gmail.com>
2024-01-14 22:59:38 +0900

    goodcondition4

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: "standalone",
+  images: {
+    domains: ['projectkeita.blob.core.windows.net'],
+  },
 }
 
 module.exports = nextConfig
